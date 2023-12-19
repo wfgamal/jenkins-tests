f30181fae89f37f7b5c0bbc3c0edffaa8562bd53
wfgamal <wfahmy@gmail.com>
2023-12-19 14:19:19 +0100

    devsecops-1

--- kubescan.sh
+++ kubescan.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
-score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | grep score | awk '{print $2}' | sed 's/,$//')
+score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | grep -i "score" | awk '{print $2}' | sed 's/,$//')
 
 
-eit_code=$?
+exit_code=$?
 
 
 if [[ "$score" -lt 6 ]];then
   echo "Your YAML file have some errors"
-  exit $exit_code
+  exit 1
 else
   echo "Your YAML files has the correct score: $score "
-  exit 0
+  exit $exit_code
 fi
